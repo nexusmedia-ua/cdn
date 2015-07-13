152dcfd0d6754858464386de9387d87e5583f13f
Denys <denis.shchotkin@gmail.com>
2015-07-13 16:30:30 +0200

    New social icons, slider update

--- easyslide/easyslide.js
+++ easyslide/easyslide.js
@@ -23,7 +23,7 @@ var jQuery191;
 loadScript("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js", function(){
     jQuery191 = jQuery.noConflict(true);
     loadScript("//nexusmedia-ua.github.io/cdn/easyslide/jquery191-ui-1.10.4.min.js", function(){
-        jQuery191(".product-description-slider").accordion( { heightStyle: "content", animate: 200 } );
-        jQuery191(".product-description-slider .ui-accordion-header").bind("click",function(){ theOffset = jQuery191(this).parent().offset().top; jQuery191("html, body").animate({scrollTop: (theOffset-50)}, 200);   });
+        jQuery191(".product-description-slider").accordion( { heightStyle: "content", animate: 100 } );
+        jQuery191(".product-description-slider .ui-accordion-header").bind("click",function(){ theOffset = jQuery191(this).parent().offset().top; jQuery191("html, body").animate({scrollTop: (theOffset-50)}, 100).finish(100);   });
     });
 });
