f6b764019480f029405263794e9210270ad3bca5
Denys <denis.shchotkin@gmail.com>
2015-07-31 14:01:56 +0200

    Opera fix added to EasySlide app JS

--- easyslide/easyslide.js
+++ easyslide/easyslide.js
@@ -25,5 +25,11 @@ loadScript("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js", functio
     loadScript("//nexusmedia-ua.github.io/cdn/easyslide/jquery191-ui-1.10.4.min.js", function(){
         jQuery191(".product-description-slider").accordion( { heightStyle: "content", animate: 100 } );
         jQuery191(".product-description-slider .ui-accordion-header").bind("click",function(){ theOffset = jQuery191(this).parent().offset().top; jQuery191("html, body").animate({scrollTop: (theOffset-50)}, 100).finish(100);   });
+        if (navigator.userAgent.indexOf("Opera") != -1) {
+          setTimeout(function(){
+            jQuery191(".product-description-slider").accordion( { heightStyle: "content", animate: 100 } );
+            jQuery191(".product-description-slider .ui-accordion-header").bind("click",function(){ theOffset = jQuery191(this).parent().offset().top; jQuery191("html, body").animate({scrollTop: (theOffset-50)}, 100).finish(100);   });
+          }, 100);
+        }
     });
 });
