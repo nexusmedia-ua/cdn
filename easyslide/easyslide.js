78a53b35b59d2386236fd621cba5162044e1f6d9
Denys <denis.shchotkin@gmail.com>
2015-10-15 21:56:24 +0200

    Collapsible update

--- easyslide/easyslide.js
+++ easyslide/easyslide.js
@@ -27,7 +27,7 @@ loadScript("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js", functio
         jQuery191(".product-description-slider .ui-accordion-header").bind("click",function(){ theOffset = jQuery191(this).parent().offset().top; jQuery191("html, body").animate({scrollTop: (theOffset-50)}, 100).finish(100);   });
         if (navigator.userAgent.indexOf("Opera") != -1) {
           setTimeout(function(){
-            jQuery191(".product-description-slider").accordion( { heightStyle: "content", animate: 100 } );
+            jQuery191(".product-description-slider").accordion( { collapsible: true, heightStyle: "content", animate: 100 } );
             jQuery191(".product-description-slider .ui-accordion-header").bind("click",function(){ theOffset = jQuery191(this).parent().offset().top; jQuery191("html, body").animate({scrollTop: (theOffset-50)}, 100).finish(100);   });
           }, 100);
         }
