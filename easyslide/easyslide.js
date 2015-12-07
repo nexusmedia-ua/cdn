59984486847c87e4a672c89067a77edef7f2650b
Denys <denis.shchotkin@gmail.com>
2015-12-07 17:33:51 +0100

    1

--- easyslide/easyslide.js
+++ easyslide/easyslide.js
@@ -24,17 +24,17 @@ loadScript("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js", functio
     jQuery191 = jQuery.noConflict(true);
     loadScript("//nexusmedia-ua.github.io/cdn/easyslide/jquery191-ui-1.10.4.min.js", function(){
         if (jQuery191("#easyslide_all_closed").size() > 0 && jQuery191("#easyslide_all_closed").val() == "1") {
-            jQuery191(".product-description-slider").accordion( { active: false, collapsible: true, heightStyle: "content", animate: 500 } );
+            jQuery191(".product-description-slider").accordion( { active: false, collapsible: true, heightStyle: "content", animate: 100 } );
         } else {
-            jQuery191(".product-description-slider").accordion( { collapsible: true, heightStyle: "content", animate: 500 } );
+            jQuery191(".product-description-slider").accordion( { collapsible: true, heightStyle: "content", animate: 100 } );
         }
         jQuery191(".product-description-slider .ui-accordion-header").bind("click",function(){ theOffset = jQuery191(this).parent().offset().top; jQuery191("html, body").animate({scrollTop: (theOffset-50)}, 100).finish(100);   });
         if (navigator.userAgent.indexOf("Opera") != -1) {
           setTimeout(function(){
             if (jQuery191("#easyslide_all_closed").size() > 0 && jQuery191("#easyslide_all_closed").val() == "1") {
-                jQuery191(".product-description-slider").accordion( { active: false, collapsible: true, heightStyle: "content", animate: 500 } );
+                jQuery191(".product-description-slider").accordion( { active: false, collapsible: true, heightStyle: "content", animate: 100 } );
             } else {
-                jQuery191(".product-description-slider").accordion( { collapsible: true, heightStyle: "content", animate: 500 } );
+                jQuery191(".product-description-slider").accordion( { collapsible: true, heightStyle: "content", animate: 100 } );
             }
             jQuery191(".product-description-slider .ui-accordion-header").bind("click",function(){ theOffset = jQuery191(this).parent().offset().top; jQuery191("html, body").animate({scrollTop: (theOffset-50)}, 100).finish(100);   });
           }, 100);
