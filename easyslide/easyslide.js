8697e60c7e213b44c87529cecb0bd21b890331c8
Denys <denis.shchotkin@gmail.com>
2015-12-09 14:56:54 +0100

    EasyAccordion scroll to accordion

--- easyslide/easyslide.js
+++ easyslide/easyslide.js
@@ -28,7 +28,7 @@ loadScript("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js", functio
         } else {
             jQuery191(".product-description-slider").accordion( { collapsible: true, heightStyle: "content", animate: 100 } );
         }
-        jQuery191(".product-description-slider .ui-accordion-header").bind("click",function(){ theOffset = jQuery191(this).parent().offset().top; jQuery191("html, body").animate({scrollTop: (theOffset-50)}, 100).finish(100);   });
+        if (jQuery191("#scroll_to_accordion").val() == "1") jQuery191(".product-description-slider .ui-accordion-header").bind("click",function(){ theOffset = jQuery191(this).parent().offset().top; jQuery191("html, body").animate({scrollTop: (theOffset-50)}, 100).finish(100);   });
         if (navigator.userAgent.indexOf("Opera") != -1) {
           setTimeout(function(){
             if (jQuery191("#easyslide_all_closed").size() > 0 && jQuery191("#easyslide_all_closed").val() == "1") {
@@ -36,7 +36,7 @@ loadScript("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js", functio
             } else {
                 jQuery191(".product-description-slider").accordion( { collapsible: true, heightStyle: "content", animate: 100 } );
             }
-            jQuery191(".product-description-slider .ui-accordion-header").bind("click",function(){ theOffset = jQuery191(this).parent().offset().top; jQuery191("html, body").animate({scrollTop: (theOffset-50)}, 100).finish(100);   });
+            if (jQuery191("#scroll_to_accordion").val() == "1") jQuery191(".product-description-slider .ui-accordion-header").bind("click",function(){ theOffset = jQuery191(this).parent().offset().top; jQuery191("html, body").animate({scrollTop: (theOffset-50)}, 100).finish(100);   });
           }, 100);
         }
     });
