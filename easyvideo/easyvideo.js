02ab6697b5a1f1fb3df2f7482f313443c530225b
Denys <denis.shchotkin@gmail.com>
2015-12-22 22:03:31 +0100

    w

--- easyvideo/easyvideo.js
+++ easyvideo/easyvideo.js
@@ -88,7 +88,7 @@ loadScriptEasyVideo("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
           } else {
             ev_video_is_show = false;
           }
-        }, 50);
+        }, 150);
       });
 
       ev$("img[alt*='youtu.be/']").on("click touchstart", function(){
@@ -99,9 +99,9 @@ loadScriptEasyVideo("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
           var width = ev$("#easyvideo_video").width();
           if (ev$("#easyvideo_video").parent().width() > width) width = ev$("#easyvideo_video").parent().width();
           ev$('#easyvideo_video').css("line-height","0").css("position","relative").css("z-index", "9999").html('<iframe width="100%"  src="https://www.youtube.com/embed/' + ev$(this).prop('alt').replace("https://youtu.be/", "") + '" frameborder="0" allowfullscreen></iframe>').find("iframe").attr("height", width * 0.5625);
-          setTimeout(function(){ if (is_zoom) ev_container.removeClass(zoom_class); ev$('#easyvideo_video').find("iframe").attr("height", width * 0.5625); }, 50);
+          setTimeout(function(){ if (is_zoom) ev_container.removeClass(zoom_class); ev$('#easyvideo_video').find("iframe").attr("height", width * 0.5625); }, 150);
           ev$('#easyvideo_video').show();
-          setTimeout(function(){ ev$('#easyvideo_video').show().css("display", "block", "important"); }, 50);
+          setTimeout(function(){ ev$('#easyvideo_video').show().css("display", "block", "important"); }, 150);
         }
       });
       ev$("img[alt*='youtu.be/']").filter("img[src*='" + ev_main_img_url + "_']").last().trigger('click');
@@ -114,9 +114,9 @@ loadScriptEasyVideo("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
           var width = ev$("#easyvideo_video").width();
           if (ev$("#easyvideo_video").parent().width() > width) width = ev$("#easyvideo_video").parent().width();
           ev$('#easyvideo_video').css("line-height","0").css("position","relative").css("z-index", "9999").html('<iframe width="100%"  src="https://www.youtube.com/embed/' + ev$(this).prop('alt').replace("https://www.youtube.com/watch?v=", "") + '" frameborder="0" allowfullscreen></iframe>').find("iframe").attr("height", width * 0.5625);
-          setTimeout(function(){ if (is_zoom) ev_container.removeClass(zoom_class); ev$('#easyvideo_video').find("iframe").attr("height", width * 0.5625); }, 50);
+          setTimeout(function(){ if (is_zoom) ev_container.removeClass(zoom_class); ev$('#easyvideo_video').find("iframe").attr("height", width * 0.5625); }, 150);
           ev$('#easyvideo_video').show();
-          setTimeout(function(){ ev$('#easyvideo_video').show().css("display", "block", "important"); }, 50);
+          setTimeout(function(){ ev$('#easyvideo_video').show().css("display", "block", "important"); }, 150);
         }
       });
       ev$("img[alt*='youtube.com/watch']").filter("img[src*='" + ev_main_img_url + "_']").last().trigger('click');
@@ -129,9 +129,9 @@ loadScriptEasyVideo("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
           var width = ev$("#easyvideo_video").width();
           if (ev$("#easyvideo_video").parent().width() > width) width = ev$("#easyvideo_video").parent().width();
           ev$('#easyvideo_video').css("line-height","0").css("position","relative").css("z-index", "9999").html('<iframe src="https://player.vimeo.com/video/' + ev$(this).prop('alt').replace("https://", "").replace("http://", "").replace("vimeo.com/","") + '" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>').find("iframe").attr("height", width * 0.5625);
-          setTimeout(function(){ if (is_zoom) ev_container.removeClass(zoom_class); ev$('#easyvideo_video').find("iframe").attr("height", width * 0.5625); }, 50);
+          setTimeout(function(){ if (is_zoom) ev_container.removeClass(zoom_class); ev$('#easyvideo_video').find("iframe").attr("height", width * 0.5625); }, 150);
           ev$('#easyvideo_video').show();
-          setTimeout(function(){ ev$('#easyvideo_video').show().css("display", "block", "important"); }, 50);
+          setTimeout(function(){ ev$('#easyvideo_video').show().css("display", "block", "important"); }, 150);
         }
       });
       ev$("img[alt*='//vimeo.com/']").filter("img[src*='" + ev_main_img_url + "_']").last().trigger('click');
