21b3c422504c44f466c70e494cf9b4f34d4e7ce1
Denys <denis.shchotkin@gmail.com>
2015-12-22 22:34:10 +0100

    4

--- easyvideo/easyvideo.js
+++ easyvideo/easyvideo.js
@@ -79,7 +79,7 @@ loadScriptEasyVideo("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
         }, 50);
       });
 
-      ev$(".MagicToolboxSelectorsContainer a").on("click touchend", function () {
+      ev$(".MagicToolboxSelectorsContainer a").on("click", function () {
         setTimeout(function(){
           if (!ev_video_is_show) {
             setTimeout(function(){ ev$('.MagicZoomPlus figure > img').show(); }, 50);
@@ -105,7 +105,7 @@ loadScriptEasyVideo("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
       });
       ev$("img[alt*='youtu.be/']").filter("img[src*='" + ev_main_img_url + "_']").last().trigger('click');
 
-      ev$("img[alt*='youtube.com/watch']").on("touchend", function(){
+      ev$("img[alt*='youtube.com/watch']").on("click", function(){
         if (!ev$(this).is(ev$( ev_main_img_selector ))) {
           ev_video_is_show = true;
           ev_main_img.hide();
