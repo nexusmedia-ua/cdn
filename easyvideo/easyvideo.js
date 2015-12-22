6c6edfa971e8ab4088063d6a7dcb82f090f2ff33
Denys <denis.shchotkin@gmail.com>
2015-12-22 22:55:02 +0100

    video height issue fix

--- easyvideo/easyvideo.js
+++ easyvideo/easyvideo.js
@@ -105,6 +105,8 @@ loadScriptEasyVideo("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
 
           var width = ev$("#easyvideo_video").width();
           if (ev$("#easyvideo_video").parent().width() > width) width = ev$("#easyvideo_video").parent().width();
+          if (width == 0) width = ev$("#easyvideo_video").parent().parent().width();
+          if (width == 0) width = 300;
           ev$('#easyvideo_video').css("line-height","0").css("position","relative").css("z-index", "9999").html('<iframe width="100%"  src="https://www.youtube.com/embed/' + ev$(this).prop('alt').replace("https://youtu.be/", "") + '" frameborder="0" allowfullscreen></iframe>').find("iframe").attr("height", width * 0.5625);
           setTimeout(function(){ if (is_zoom) ev_container.removeClass(zoom_class); ev$('#easyvideo_video').find("iframe").attr("height", width * 0.5625); }, 50);
 
@@ -130,6 +132,8 @@ loadScriptEasyVideo("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
 
           var width = ev$("#easyvideo_video").width();
           if (ev$("#easyvideo_video").parent().width() > width) width = ev$("#easyvideo_video").parent().width();
+          if (width == 0) width = ev$("#easyvideo_video").parent().parent().width();
+          if (width == 0) width = 300;
           ev$('#easyvideo_video').css("line-height","0").css("position","relative").css("z-index", "9999").html('<iframe width="100%"  src="https://www.youtube.com/embed/' + ev$(this).prop('alt').replace("https://www.youtube.com/watch?v=", "") + '" frameborder="0" allowfullscreen></iframe>').find("iframe").attr("height", width * 0.5625);
           setTimeout(function(){ if (is_zoom) ev_container.removeClass(zoom_class); ev$('#easyvideo_video').find("iframe").attr("height", width * 0.5625); }, 50);
 
@@ -155,6 +159,8 @@ loadScriptEasyVideo("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
 
           var width = ev$("#easyvideo_video").width();
           if (ev$("#easyvideo_video").parent().width() > width) width = ev$("#easyvideo_video").parent().width();
+          if (width == 0) width = ev$("#easyvideo_video").parent().parent().width();
+          if (width == 0) width = 300;
           ev$('#easyvideo_video').css("line-height","0").css("position","relative").css("z-index", "9999").html('<iframe src="https://player.vimeo.com/video/' + ev$(this).prop('alt').replace("https://", "").replace("http://", "").replace("vimeo.com/","") + '" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>').find("iframe").attr("height", width * 0.5625);
           setTimeout(function(){ if (is_zoom) ev_container.removeClass(zoom_class); ev$('#easyvideo_video').find("iframe").attr("height", width * 0.5625); }, 50);
 
