c6fc499e81a2c8d9c0dd18e5e2d5a4c966961546
Denys <denis.shchotkin@gmail.com>
2016-01-10 19:36:52 +0100

    111

--- easyvideo/easyvideo.js
+++ easyvideo/easyvideo.js
@@ -70,8 +70,11 @@ loadScriptEasyVideo("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
       ev$('.flexslider .slides li .video-container').each(function(){
         var video_link = ev$(this).find('a').html();
         var flexwidth = ev$(this).width();
+        console.log(flexwidth);
         if (parseInt(flexwidth) == 0) flexwidth = ev$('.flexslider').width();
+        console.log(flexwidth);
         if (parseInt(flexwidth) == 0) flexwidth = 505;
+        console.log(flexwidth);
         if ( video_link.toLowerCase().indexOf("youtube.com/watch") >= 0 ) {
           ev$(this).html('<iframe width="100%"  src="https://www.youtube.com/embed/' + video_link.replace("https://www.youtube.com/watch?v=", "").trim() + '" frameborder="0" allowfullscreen></iframe>').find("iframe").attr("height", flexwidth * 0.5625).css("height", flexwidth * 0.5625 + "px");
         }
