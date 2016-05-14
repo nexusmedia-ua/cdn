c091c2d8af366830b8a6268a53c3b3a159b4b860
Denys <denis.shchotkin@gmail.com>
2016-05-14 20:11:10 +0200

    ZoomContainer fix

--- easyvideo/easyvideo.js
+++ easyvideo/easyvideo.js
@@ -37,9 +37,11 @@ function showVideoBlock(ev$) {
     ev$('#easyvideo_video').show();
 
     // Adding delay for touch events
-    setTimeout(function(){ ev$('#easyvideo_video').show(); }, 50);
-    setTimeout(function(){ ev$('#easyvideo_video').show(); }, 150);
-    setTimeout(function(){ ev$('#easyvideo_video').show(); }, 300);
+    setTimeout(function(){ ev$('#easyvideo_video').show(); ev$('zoomContainer').css('height', '0'); }, 50);
+    setTimeout(function(){ ev$('#easyvideo_video').show(); ev$('zoomContainer').css('height', '0'); }, 150);
+    setTimeout(function(){ ev$('#easyvideo_video').show(); ev$('zoomContainer').css('height', '0'); }, 300);
+
+    ev$('zoomContainer').css('height', '0');
 }
 
 var ev$;
