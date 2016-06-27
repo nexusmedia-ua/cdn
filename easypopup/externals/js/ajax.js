f9001cf4cc82720394822536e552cf153408b769
Denys <denis.shchotkin@gmail.com>
2016-06-27 17:46:19 +0200

    easypopup notification

--- easypopup/externals/js/ajax.js
+++ easypopup/externals/js/ajax.js
@@ -1,5 +1,8 @@
+"use strict";
+
 var ajaxRequest = null;
 var ajaxIsActive = false;
+var $ajaxDisabledBlock = null;
 var backgroundAjaxRequest = null;
 
 window.onbeforeunload = function()
@@ -8,30 +11,36 @@ window.onbeforeunload = function()
     ajaxRequest.abort();
   }
   if( popupUpdated || backgroundAjaxRequest ) {
-    if( isset(ShopifyApp) ) ShopifyApp.Modal.alert("Please wait, we're saving your changes.");
+    if( isset(ShopifyApp) ) {
+      ShopifyApp.Modal.alert("Please wait, we're saving your changes.");
+      // safari hack
+      if( navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1 ) {
+        ShopifyApp.Modal.alert("Please wait, we're saving your changes.");
+      }
+    }
     if( popupUpdated ) backgroundSaveSnippet(true);
   }
 };
 
-
-function ajaxCall(url, params, dt, rt, dbl)
+function ajaxCall(url, data, config)
 {
   if( ajaxIsActive ) return false;
 
-  var dataType    = dt    || 'html';
-  var requestType = rt    || 'POST';
-  var doubleAjax  = false || dbl;
-  var ajaxParams  = {'ajax' : true};
+  var ajaxParams   = {'ajax' : true, 'ahmac' : globalAhmac};
+  var responseType = config && typeof config['response_type'] === 'string' ? config['response_type'] : 'json';
+  var requestType  = config && typeof config['request_type']  === 'string' ? config['request_type']  : 'POST';
+  var doubleAjax   = config && typeof config['double_ajax']   === 'boolean'? config['double_ajax']   : false;
+  $ajaxDisabledBlock = config && typeof config['disabled_block'] === 'string' && $(config['disabled_block']).length ? $(config['disabled_block']) : null;
 
-  $.extend(ajaxParams, params);
-  ajaxBefore(params);
+  $.extend(ajaxParams, data);
+  ajaxBefore(data);
 
   ajaxRequest = $.ajax({
-    type: requestType,
-    url: url,
+    type : requestType,
+    url  : url,
     async: true,
-    dataType: dataType,
-    data: ajaxParams,
+    dataType: responseType,
+    data : ajaxParams,
     complete: function(response) {
       if( !doubleAjax ) ajaxAfter(response);
     }
@@ -40,14 +49,101 @@ function ajaxCall(url, params, dt, rt, dbl)
   return ajaxRequest;
 }
 
+function ajaxCallForm(url, form, config)
+{
+  if( ajaxIsActive ) return false;
+
+  var responseType   = config && typeof config['response_type']  === 'string' ? config['response_type'] : 'json';
+  $ajaxDisabledBlock = config && typeof config['disabled_block'] === 'string' && $(config['disabled_block']).length ? $(config['disabled_block']) : null;
+
+  form.append('ajax',  1);
+  form.append('ahmac', globalAhmac);
+  ajaxBefore(config);
+
+  ajaxRequest = $.ajax({
+    type: 'POST',
+    url: url,
+    dataType: responseType,
+    data: form,
+    processData: false,
+    contentType: false,
+    complete: function(response) {
+      ajaxAfter(response);
+    }
+  })
+
+  return ajaxRequest;
+}
+
+function ajaxBefore(params)
+{
+  var params = params || {};
+  ajaxIsActive = true;
+
+  if( $ajaxDisabledBlock && $ajaxDisabledBlock.length ) $ajaxDisabledBlock.addClass('easypopup-disabled');
+  $('.grid-stack-item.ui-draggable').draggable('disable');
+  $('.mce-menu').hide();
+
+  if( params && isset(params.task) && params.task == 'ajax_layout_editor' && params.action != 'get_templates' ) {
+    var syncNotice = $('#sync-layout-notice');
+    if( syncNotice.length && syncNotice.is(":visible") ) {
+      syncNotice.remove();
+      $('#layout-editor-tabs .active .fa-link').remove();
+      $('#unsync-layout-notice').show();
+    }
+  }
+}
+
+function ajaxAfter(response)
+{
+  if( !ajaxCheckResponse(response) ) return;
+
+  $('div.loader').hide();
+  $('.grid-stack-item.ui-draggable').draggable('enable');
+  if( $ajaxDisabledBlock && $ajaxDisabledBlock.length ) $ajaxDisabledBlock.removeClass('easypopup-disabled');
+
+  ajaxIsActive = false;
+  ajaxRequest = null;
+  $ajaxDisabledBlock = null;
+}
+
+function ajaxCheckResponse(response)
+{
+  if( globalAppCurrentUrl ) {
+    if( typeof(response) == 'string' && response == 'invalid shop' ) {
+      window.top.location.href = globalAppCurrentUrl;
+      return false;
+    } else if( typeof(response) == 'object' && isset(response.responseText) && response.responseText == 'invalid shop' ) {
+      window.top.location.href = globalAppCurrentUrl;
+      return false;
+    }
+  }
+  return true;
+}
+
+function backgroundCheckPopupToUpdate()
+{
+  $.ajax({
+    type: 'POST',
+    url: globalBaseUrl,
+    dataType: 'json',
+    data: {'ajax' : true, 'task': 'ajax_layout_actions', 'action': 'check_to_update'},
+    success: function(response) {
+      if( response.data ) $('#popup-save-inprogress').show();
+      else $('#popup-save-inprogress').hide();
+    }
+  });
+}
 
 function backgroundSaveSnippet(showLoader)
 {
   var dataType    = 'json';
   var requestType = 'POST';
-  var ajaxParams  = {'ajax' : true, 'task' : 'ajax_layout_editor', 'action': 'update_snippet', 'content_id': layoutId};
+  var ajaxParams  = {'ajax' : true, 'ahmac' : globalAhmac, 'task' : 'ajax_layout_editor', 'action': 'update_snippet', 'content_id': layoutId};
   if( showLoader ) $('#layout-editor-loader').show();
+
   popupUpdated = false;
+  $('#popup-save-inprogress').hide();
 
   backgroundAjaxRequest = $.ajax({
     type: requestType,
@@ -63,70 +159,4 @@ function backgroundSaveSnippet(showLoader)
   });
 
   return backgroundAjaxRequest;
-}
-
-
-function ajaxCallForm(url, form, dt)
-{
-  if( ajaxIsActive ) return false;
-
-  var dataType = dt || 'html';
-  form.append('ajax',  1);
-  ajaxBefore();
-
-  ajaxRequest = $.ajax({
-    type: 'POST',
-    url: url,
-    dataType: dataType,
-    data: form,
-    processData: false,
-    contentType: false,
-    complete: function(response) {
-      ajaxAfter(response);
-    }
-  })
-  return ajaxRequest;
-}
-
-
-function ajaxBefore(params)
-{
-  var params = params || '';
-  ajaxIsActive = true;
-
-  $('#layout-row, #editor-sidebar-form-container .column-menu').addClass('easypopup-disabled');
-  $('.grid-stack-item.ui-draggable').draggable('disable');
-
-  var syncNotice = $('#sync-layout-notice');
-
-  if( syncNotice.length && syncNotice.is(":visible") && params && isset(params.task) && params.task == 'ajax_layout_editor' ) {
-    syncNotice.remove();
-    $('#unsync-layout-notice').show();
-  }
-}
-
-function ajaxAfter(response)
-{
-  if( !ajaxCheckResponse(response) ) return;
-
-  $('div.loader').hide();
-
-  $('.grid-stack-item.ui-draggable').draggable('enable');
-  $('#layout-row, #editor-sidebar-form-container .column-menu').removeClass('easypopup-disabled');
-  ajaxIsActive = false;
-  ajaxRequest = null;
-}
-
-function ajaxCheckResponse(response)
-{
-  if( globalAppCurrentUrl ) {
-    if( typeof(response) == 'string' && response == 'invalid shop' ) {
-      window.top.location.href = globalAppCurrentUrl;
-      return false;
-    } else if( typeof(response) == 'object' && isset(response.responseText) && response.responseText == 'invalid shop' ) {
-      window.top.location.href = globalAppCurrentUrl;
-      return false;
-    }
-  }
-  return true;
 }
