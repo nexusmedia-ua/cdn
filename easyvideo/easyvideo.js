dcafb2594d3422886bec9e09a8b74d4f3ac02fab
Denys <denis.shchotkin@gmail.com>
2016-06-30 02:17:33 +0200

    EasyVideo adj

--- easyvideo/easyvideo.js
+++ easyvideo/easyvideo.js
@@ -57,7 +57,7 @@ loadScriptEasyVideo("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
     function easyVideoSetup() {
 
       // Fix for Owl Slider on PhotoBooth theme
-      ev$("#product-images .owl-item div img[alt*='youtube.com/watch']").each(function(){
+      ev$("#product-images .owl-item div img[alt*='youtube.com/watch'], #mob-product-images .owl-item div img[alt*='youtube.com/watch").each(function(){
         var video_link = ev$(this).prop("alt");
         ev$(this).parent().html('<iframe width="100%"  src="https://www.youtube.com/embed/' + video_link.replace("https://www.youtube.com/watch?v=", "").trim() + '" frameborder="0" allowfullscreen></iframe>').find("iframe").attr("height", ev$("#product-images").width() * 0.5625).css("height", ev$("#product-images").width() * 0.5625 + "px");
         if ( ev$('#easyvideo_video').find("iframe").width() > 0) {
@@ -65,7 +65,7 @@ loadScriptEasyVideo("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
           ev$('#easyvideo_video').find("iframe").attr("height", video_width * 0.5625);
         }
       });
-      ev$("#product-images .owl-item div img[alt*='youtu.be/']").each(function(){
+      ev$("#product-images .owl-item div img[alt*='youtu.be/'], #mob-product-images .owl-item div img[alt*='youtu.be/']").each(function(){
         var video_link = ev$(this).prop("alt");
         ev$(this).parent().html('<iframe width="100%"  src="https://www.youtube.com/embed/' + video_link.replace("https://youtu.be/", "").trim() + '" frameborder="0" allowfullscreen></iframe>').find("iframe").attr("height", ev$("#product-images").width() * 0.5625).css("height", ev$("#product-images").width() * 0.5625 + "px");
         if ( ev$('#easyvideo_video').find("iframe").width() > 0) {
@@ -73,7 +73,7 @@ loadScriptEasyVideo("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
           ev$('#easyvideo_video').find("iframe").attr("height", video_width * 0.5625);
         }
       });
-      ev$("#product-images .owl-item div img[alt*='//vimeo.com/']").each(function(){
+      ev$("#product-images .owl-item div img[alt*='//vimeo.com/'], #mob-product-images .owl-item div img[alt*='//vimeo.com/']").each(function(){
         var video_link = ev$(this).prop("alt");
         ev$(this).parent().html('<iframe src="https://player.vimeo.com/video/' + video_link.replace("https://", "").replace("http://", "").replace("vimeo.com/","").trim() + '" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>').find("iframe").attr("height", ev$("#product-images").width() * 0.5625).css("height", ev$("#product-images").width() * 0.5625 + "px");
         if ( ev$('#easyvideo_video').find("iframe").width() > 0) {
