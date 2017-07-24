a7246b8344575e741ffaaa0d005829696e47e629
Denys <denis.shchotkin@gmail.com>
2017-07-24 09:18:17 +0200

    easy video new theme support

--- easyvideo/easyvideo.js
+++ easyvideo/easyvideo.js
@@ -179,6 +179,24 @@ ev$(function(){
         }
       });
 
+      // Fix for Brooklyn theme
+      ev$('.flexslider .slides li.slide img').each(function(){
+        var video_link = ev$(this).attr('alt');
+        if (video_link) {
+          flexwidth = ev$(this).parent().parent().width();
+          if (parseInt(flexwidth) == 0) flexwidth = 505;
+          if ( video_link.toLowerCase().indexOf("youtube.com/watch") >= 0 ) {
+            ev$(this).parent().html('<iframe width="100%"  src="https://www.youtube.com/embed/' + video_link.replace("https://www.youtube.com/watch?v=", "").trim().split('&')[0].split('?')[0] + youtube_parameters.replace('VIDEOID', video_link.replace("https://www.youtube.com/watch?v=", "").trim().split('&')[0].split('?')[0]) + '" frameborder="0" allowfullscreen></iframe>').find("iframe").attr("height", Math.round(flexwidth * ev_dimensions)).css("height", Math.round(flexwidth * ev_dimensions) + "px");
+          }
+          if ( video_link.toLowerCase().indexOf("youtu.be/") >= 0 ) {
+            ev$(this).parent().html('<iframe width="100%"  src="https://www.youtube.com/embed/' + video_link.replace("https://youtu.be/", "").trim().split('&')[0].split('?')[0] + youtube_parameters.replace('VIDEOID', video_link.replace("https://www.youtube.com/watch?v=", "").trim().split('&')[0].split('?')[0]) + '" frameborder="0" allowfullscreen></iframe>').find("iframe").attr("height", Math.round(flexwidth * ev_dimensions)).css("height", Math.round(flexwidth * ev_dimensions) + "px");
+          }
+          if ( video_link.toLowerCase().indexOf("//vimeo.com/") >= 0 ) {
+            ev$(this).parent().html('<iframe src="https://player.vimeo.com/video/' + video_link.replace("https://", "").replace("http://", "").replace("vimeo.com/","").trim().split('&')[0].split('?')[0] + vimeo_parameters + '" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>').find("iframe").attr("height", Math.round(flexwidth * ev_dimensions)).css("height", Math.round(flexwidth * ev_dimensions) + "px");
+          }
+        } 
+      });
+
       ev$('.flickity-slider > .gallery-cell > div').each(function(){
         var video_link = ev$(this).find('a').html();
         flexwidth = ev$('.flickity-slider').width();
