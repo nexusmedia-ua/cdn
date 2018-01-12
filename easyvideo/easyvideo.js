b7a3269ec433b65973daf3338e4af29ca8429bd7
Denys <denis.shchotkin@gmail.com>
2018-01-12 21:53:07 +0100

    ...

--- easyvideo/easyvideo.js
+++ easyvideo/easyvideo.js
@@ -284,12 +284,12 @@ ev$(function(){
         }
       });       
 
-      // Fix for Debut theme
-      ev$('.product-single > .grid__item .feature-row__image').each(function(){
+      // Fix for Debut, Minimal theme
+      ev$('.product-single > .grid__item .feature-row__image, .product-single > .product-single__hero .product-single__image-wrapper > .product-single__image').each(function(){
         var video_link = ev$(this).attr('alt');
         flexwidth = ev$(this).parent().parent().width();
         if (parseInt(flexwidth) == 0) flexwidth = 505;
-        ev$(this).parent().css('overflow', "vidible").css("height", "auto").addClass('ev-no-force-height');
+        ev$(this).parent().css('overflow', "vidible").css("height", "auto");
         if ( video_link.toLowerCase().indexOf("youtube.com/watch") >= 0 ) {
           ev$(this).parent().html('<iframe width="100%"  src="https://www.youtube.com/embed/' + video_link.replace("https://www.youtube.com/watch?v=", "").trim().split('&')[0].split('?')[0] + youtube_parameters.replace('VIDEOID', video_link.replace("https://www.youtube.com/watch?v=", "").trim().split('&')[0].split('?')[0]) + '" frameborder="0" allowfullscreen></iframe>').find("iframe").attr("height", Math.round(flexwidth * ev_dimensions)).css("height", Math.round(flexwidth * ev_dimensions) + "px");
         }
