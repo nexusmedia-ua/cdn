ba548d697775495f8f70ce0a4cc046b9af9d43f7
Denys <denis.shchotkin@gmail.com>
2018-01-16 19:58:18 +0100

    ...

--- easyvideo/easyvideo.js
+++ easyvideo/easyvideo.js
@@ -287,7 +287,8 @@ ev$(function(){
       // Fix for Simple theme
       ev$('.product-single .product-single__featured-image-wrapper .product-single__photos img').each(function(){
         var video_link = ev$(this).attr('alt');
-        flexwidth = ev$(this).parent().width();
+        flexwidth = ev$(this).parent().parent().width();
+        console.log(flexwidth);
         if (parseInt(flexwidth) == 0) flexwidth = 505;
         ev$(this).parent().css('overflow', "vidible").css("height", "auto");
         if ( video_link.toLowerCase().indexOf("youtube.com/watch") >= 0 ) {
